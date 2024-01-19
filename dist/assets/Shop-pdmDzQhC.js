9ed6f36cbadee6c77b5bd24694ae935a5b4a98df
GowthamSKdev <gowthams2647@gmail.com>
2024-01-19 21:39:03 +0530

    secound commit

--- dist/assets/Shop-pdmDzQhC.js
+++
@@ -1,7 +0,0 @@
-import{r as o,_ as s,j as r}from"./index-I8GDrITg.js";const t=o.lazy(()=>s(()=>import("./Hero-fpIP86fU.js"),__vite__mapDeps([0,1,2,3]))),_=o.lazy(()=>s(()=>import("./Popular-x3Ix-jo6.js"),__vite__mapDeps([4,1,2,5]))),e=o.lazy(()=>s(()=>import("./Offer-enFZnc_q.js"),__vite__mapDeps([6,1,2,7]))),i=()=>r.jsx(r.Fragment,{children:r.jsxs("div",{className:"Shop-container",children:[r.jsx(t,{}),r.jsx(_,{}),r.jsx(e,{})]})});export{i as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/Hero-fpIP86fU.js","assets/index-I8GDrITg.js","assets/index--8TqK9Q7.css","assets/Hero-jlJKK_1V.css","assets/Popular-x3Ix-jo6.js","assets/Popular-uElcsaCf.css","assets/Offer-enFZnc_q.js","assets/Offer-Xaa_cvI6.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
