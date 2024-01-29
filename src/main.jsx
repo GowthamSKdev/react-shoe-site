7bdd3fa234a2493b829a8fdb6e7062ccb272e0de
GowthamSKdev <145574815+GowthamSKdev@users.noreply.github.com>
2024-01-29 17:22:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ShopContextProvider from './context/ShopContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import ShopContextProvider from './context/ShopContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
+  <BrowserRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-)
+  </BrowserRouter>,
+  document.getElementById( 'root' )
+);
