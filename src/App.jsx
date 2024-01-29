c7b0379555d31d16ec923c8761d6955a243f1762
GowthamSKdev <145574815+GowthamSKdev@users.noreply.github.com>
2024-01-29 17:21:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { DotLoader } from 'react-spinners';
 
-const Shop = lazy(() => import("./pages/Shop"));
-const Navbar = lazy(() => import("./components/Navbar"));
-const ShopCatagory = lazy(() => import("./pages/ShopCatagory"));
-const Product = lazy(() => import("./pages/Product"));
-const LoginSignup = lazy(() => import("./pages/LoginSignup"));
-const Cart = lazy(() => import("./pages/Cart"));
-const Footer = lazy(() => import("./components/Footer"))
 
-function App() {
 
+const Shop = lazy( () => import( "./pages/Shop" ) );
+const Navbar = lazy( () => import( "./components/Navbar" ) );
+const ShopCatagory = lazy( () => import( "./pages/ShopCatagory" ) );
+const Product = lazy( () => import( "./pages/Product" ) );
+const LoginSignup = lazy( () => import( "./pages/LoginSignup" ) );
+const Cart = lazy( () => import( "./pages/Cart" ) );
+const Footer = lazy( () => import( "./components/Footer" ) );
+
+function App ()
+{
   return (
     <>
-        {/* <Suspense fallback={<DotLoader color="#000" className='loader' />} >
-        </Suspense> */}
-      <BrowserRouter>
-        <Navbar />
+      <main id='main'>
+        <Suspense fallback={<DotLoader color="#000" className='loader' />}>
+          <Navbar />
           <Routes>
             <Route path='/' element={<Shop />} />
             <Route path='/men' element={<ShopCatagory catagory="men" />} />
@@ -30,14 +31,14 @@ function App() {
             </Route>
 
             <Route path='/login' element={<LoginSignup Type='Login' />} />
-            <Route path='/Signup' element={<LoginSignup Type='Signup' />} />
+            <Route path='/signup' element={<LoginSignup Type='Signup' />} />
             <Route path='/cart' element={<Cart />} />
-
           </Routes>
-        <Footer />
-      </BrowserRouter>
+          <Footer />
+        </Suspense>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
